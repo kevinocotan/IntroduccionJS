aef4b81b4e78436ac5944ff2d4c4fc68a3ed23ae
kevinocotan <kaocotan@gmail.com>
2024-12-04 15:26:20 -0600

    object freeze y object seal

--- js/05.js
+++ js/05.js
@@ -6,6 +6,9 @@ const producto = {
     disponible : false
 }
 
+// Object.freeze(producto) // Sobre el objeto que se esta aplicando, no va a dejar modificarlo. Quiere decir que todo lo que hago aca abajo, no se modifica.
+Object.seal(producto) // Permite modificar las existentes, pero no permite añadir.
+
 // Reescribir un valor
 producto.disponible = true
 
