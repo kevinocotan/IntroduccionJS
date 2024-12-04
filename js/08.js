5ac1fa0d36dfa8e7d16667eabf88660c3a7049fd
kevinocotan <kaocotan@gmail.com>
2024-12-04 16:40:47 -0600

    template strings concatenacion

--- js/08.js
+++ js/08.js
@@ -4,5 +4,15 @@ const producto = "Tablet de 12 Pulgadas"
 const precio = 400
 const marca = "Orange"
 
-console.log('El Producto es: ' + producto)
+// Ejemplo sencillo
+
+console.log('El Producto es: ' + producto) // Esto es lo mismo
+
+console.log(`El producto es: ${producto}`) //Que esto.
+
+// Ejemplo normal
+
+console.log(producto + " $" + precio + " Dolares, marca: " + marca)
+
+console.log(`${producto} $${precio} Dolares, marca: ${marca}`) // La nueva forma de concatenar
 
