01de115229142b0621d83f0e8150d0e5793a43ee
kevinocotan <kaocotan@gmail.com>
2024-12-04 12:41:57 -0600

    Objetos

---
+++ js/04.js
@@ -0,0 +1,13 @@
+/* Objetos */
+/* Es una colección de propiedades. */
+
+const producto = {
+    nombre : "Tablet", // Propiedad es todo. "nombre" es key, y "Tablet" es value.
+    precio : 300, // Las comas separan las propiedades.
+    disponible : false
+}
+
+console.log(producto)
+console.table(producto)
+console.log(typeof producto)
+
