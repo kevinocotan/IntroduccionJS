d6242aa8d26c6d4f1399b6e3b6d5392258b693cb
kevinocotan <kaocotan@gmail.com>
2024-12-04 12:26:50 -0600

    tipo de dato, bigint

--- js/03.js
+++ js/03.js
@@ -20,13 +20,19 @@
 
 // Numbers
 
-const numero = 20.20
-const numero2 = 30
-const numero3 = -100
-const numero4 = "20" // Esto es un string, por que lleva comillas.
+// const numero = 20.20
+// const numero2 = 30
+// const numero3 = -100
+// const numero4 = "20" // Esto es un string, por que lleva comillas.
 
-console.log(typeof numero)
-console.log(typeof numero2)
-console.log(typeof numero3)
-console.log(typeof numero4) // Resultado: Un string, por que lleva comillas.
-console.log(numero4 + numero2) // Intentara sumar un string con un number. Resultado: 2030 
+// console.log(typeof numero)
+// console.log(typeof numero2)
+// console.log(typeof numero3)
+// console.log(typeof numero4) // Resultado: Un string, por que lleva comillas.
+// console.log(numero4 + numero2) // Intentara sumar un string con un number. Resultado: 2030 
+
+//BigInt
+
+const numeroGrande = BigInt(12312415443534512413626787907879679)
+
+console.log(typeof numeroGrande)
