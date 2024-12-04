3cf06b94f3ec00cc6d374651273bfe6401dbed74
kevinocotan <kaocotan@gmail.com>
2024-12-04 13:12:26 -0600

    comparativo con el destructuring

--- js/04.js
+++ js/04.js
@@ -11,5 +11,17 @@ const producto = {
 // console.table(producto)
 // console.log(typeof producto)
 
-console.log(producto.nombre) // Sintaxis de punto.
+// console.log(producto.nombre) // Sintaxis de punto.
 
+
+/* Destructuring */
+
+const { nombre } = producto // Ambos bloques de codigo hacen lo mismo.
+console.log(nombre)
+
+const nombre = producto.nombre // Ambos bloques de codigo hacen lo mismo.
+const precio = producto.precio
+const disponible = producto.disponible
+console.log(nombre)
+console.log(precio)
+console.log(disponible)
