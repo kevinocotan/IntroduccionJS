03f618179c070084e5c9990cbfa8f3fa3549afde
kevinocotan <kaocotan@gmail.com>
2024-12-04 11:53:26 -0600

    tipo de dato, strings o cadenas de texto

--- js/03.js
+++ js/03.js
@@ -1,6 +1,19 @@
 // Tipos de Dato
 
 // Undefined
-let cliente;
-console.log(cliente);
-console.log(typeof cliente);
+
+// let cliente;
+// console.log(cliente);
+// console.log(typeof cliente);
+
+// Strings o Cadenas de Texto
+
+let alumno = "Juan"
+
+console.log(alumno)
+console.log(typeof alumno)
+
+const producto = 'Monitor de 49 Pulgadas'
+
+console.log(producto)
+console.log(typeof producto)
