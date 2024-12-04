d72a9152875f1851abfc7d1439c6b544558eac3c
kevinocotan <kaocotan@gmail.com>
2024-12-04 11:59:06 -0600

    tipos de datos, numbers

--- js/03.js
+++ js/03.js
@@ -8,12 +8,25 @@
 
 // Strings o Cadenas de Texto
 
-let alumno = "Juan"
+// let alumno = "Juan"
 
-console.log(alumno)
-console.log(typeof alumno)
+// console.log(alumno)
+// console.log(typeof alumno)
 
-const producto = 'Monitor de 49 Pulgadas'
+// const producto = 'Monitor de 49 Pulgadas'
 
-console.log(producto)
-console.log(typeof producto)
+// console.log(producto)
+// console.log(typeof producto)
+
+// Numbers
+
+const numero = 20.20
+const numero2 = 30
+const numero3 = -100
+const numero4 = "20" // Esto es un string, por que lleva comillas.
+
+console.log(typeof numero)
+console.log(typeof numero2)
+console.log(typeof numero3)
+console.log(typeof numero4) // Resultado: Un string, por que lleva comillas.
+console.log(numero4 + numero2) // Intentara sumar un string con un number. Resultado: 2030 
