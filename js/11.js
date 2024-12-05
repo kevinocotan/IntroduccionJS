bf7998e54a0b284378f6a61e17ec666847c303c4
kevinocotan <kaocotan@gmail.com>
2024-12-05 14:56:04 -0600

    destructuring de arrays

---
+++ js/11.js
@@ -0,0 +1,9 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js"];
+
+const react = tecnologias [3]
+
+// Destructuring de arrays.
+
+const [ , , ,  reactjs] = tecnologias
+
+console.log(reactjs)
