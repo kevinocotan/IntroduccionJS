e211195b3d563951a0f6df5ca4637197112d760d
kevinocotan <kaocotan@gmail.com>
2024-12-05 15:39:41 -0600

    funciones

---
+++ js/13.js
@@ -0,0 +1,19 @@
+// Function declaration
+
+// function sumar () {
+//     console.log(1 + 1)
+// } // DECLARACION DE FUNCION
+
+// function sumar2 () {
+//     console.log(2 + 2)
+// }
+
+// sumar() // LLAMAR A LA FUNCION
+// sumar2()
+
+function sumar (numero1, numero2) {
+    console.log(numero1 + numero2)
+}
+
+sumar(10, 20)
+
