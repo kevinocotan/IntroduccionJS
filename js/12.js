eef5ea0fa5f017660c5b8d78c448f6632c7a6a5c
kevinocotan <kaocotan@gmail.com>
2024-12-05 15:16:55 -0600

    foreach, map, for of

--- js/12.js
+++ js/12.js
@@ -1,4 +1,11 @@
-const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Next.js"];
+const tecnologias = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React.js",
+  "Node.js",
+  "Next.js",
+];
 
 // console.log(tecnologias[0])
 // console.log(tecnologias[1])
@@ -6,6 +13,29 @@ const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Next.j
 // console.log(tecnologias[3])
 // console.log(tecnologias[4]) // Esto nunca se hace.
 
-for(let i = 0; i < 6; i++) {
-    console.log(tecnologias[i])
+// for(let i = 0; i < 6; i++) {
+//     console.log(tecnologias[i])
+// }
+
+// foreach
+
+// const arrayforEach = tecnologias.forEach(function (tech) {
+//   console.log(tech);
+// });
+
+// map
+
+const arrayMap = tecnologias.map(function (tech) {
+  return tech;
+});
+
+// diferencia entre un foreach  un map. Es que el map te genera un nuevo arreglo.
+
+// for .. of
+
+for(let tech of tecnologias) {
+    console.log(tech)
 }
+
+// console.log(arrayforEach);
+// console.log(arrayMap);
