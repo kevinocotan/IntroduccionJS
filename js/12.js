b0e277ca3f8a4ed262b6957d14112ac7ff2f5864
kevinocotan <kaocotan@gmail.com>
2024-12-05 15:05:38 -0600

    arrays con for

---
+++ js/12.js
@@ -0,0 +1,11 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Next.js"];
+
+// console.log(tecnologias[0])
+// console.log(tecnologias[1])
+// console.log(tecnologias[2])
+// console.log(tecnologias[3])
+// console.log(tecnologias[4]) // Esto nunca se hace.
+
+for(let i = 0; i < 6; i++) {
+    console.log(tecnologias[i])
+}
