4bb2893d4ebadb7bcd762642fa4c9af6d349db39
kevinocotan <kaocotan@gmail.com>
2024-12-05 16:47:55 -0600

    con function expression

---
+++ js/16.js
@@ -0,0 +1,16 @@
+//Funciones que retornan valores.
+
+const sumar = function (numero1 = 0, numero2 = 0) {
+  return numero1 + numero2;
+};
+
+// function sumar(numero1 = 0, numero2 = 0) {
+//   console.log(numero1 + numero2);
+
+// }
+
+// const sumar = (numero1 = 0, numero2 = 0) => console.log(numero1 + numero2);
+
+const resultado = sumar(10, 20);
+
+console.log(resultado)
