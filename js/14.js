c41501d7272c164641382e975ae642790d2ef139
kevinocotan <kaocotan@gmail.com>
2024-12-05 15:58:12 -0600

    function expression

---
+++ js/14.js
@@ -0,0 +1,16 @@
+// Funciones - Funcion Expression
+
+sumar(10, 20);
+sumar(100, 1);
+sumar(100);
+
+// const sumar = function (numero1 = 0, numero2 = 0) {
+//   console.log(numero1 + numero2);
+// };
+
+function sumar(numero1 = 0, numero2 = 0) {
+  console.log(numero1 + numero2); //Aqui no importa el orden pq primero registrar primero todas las funciones, y en segundo llamado empieza a ejecutar las funciones.
+
+}
+
+
