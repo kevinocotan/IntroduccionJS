81dd9d89bc9a6fec067c98f0d71306435caf8845
kevinocotan <kaocotan@gmail.com>
2024-12-05 17:00:29 -0600

    array methods

---
+++ js/17.js
@@ -0,0 +1,8 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js"];
+const numeros = [10, 20, 30]
+
+//Filter
+
+const nuevoArray = tecnologias.filter((tech) => tech !== 'HTML')
+
+console.log(nuevoArray)
