194f87c9bd14a1076f4e6dd58c2af1ead2066066
kevinocotan <kaocotan@gmail.com>
2024-12-05 14:40:56 -0600

    modificar arrays

---
+++ js/10.js
@@ -0,0 +1,9 @@
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js']
+
+tecnologias.push('Next.js')
+
+const nuevoArreglo = [...tecnologias, 'Next.js']
+
+console.table(tecnologias)
+
+console.log(nuevoArreglo)
