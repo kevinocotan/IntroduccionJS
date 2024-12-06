460f4aa1de84ab24d0af90857cbe0b30fcf1d036
kevinocotan <kaocotan@gmail.com>
2024-12-06 11:22:05 -0600

    array method find

--- js/17.js
+++ js/17.js
@@ -13,9 +13,15 @@ const numeros = [10, 20, 30]
 
 // Some  - Devuelve si al menos uno cumple la condición
 
-const resultado = numeros.some (numero => numero > 15)
-if (resultado){
-    console.log('Si hay elementos')
-} else {
-    console.log('No hay elementos')
-}
+// const resultado = numeros.some (numero => numero > 15)
+// if (resultado){
+//     console.log('Si hay elementos')
+// } else {
+//     console.log('No hay elementos')
+// }
+
+// Find - Devuelve el primer elemento que cumple una condicion, 30 no lo devuelve pq es el segundo elemento
+
+const resultado = numeros.find (numero => numero > 15)
+ 
+console.log(resultado)
