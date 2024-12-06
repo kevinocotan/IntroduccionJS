935f956663f46f2f8b68f57861dce18db3afbd67
kevinocotan <kaocotan@gmail.com>
2024-12-06 12:15:30 -0600

    Nullish coalescing operator

--- js/22.js
+++ js/22.js
@@ -14,3 +14,7 @@ console.log(alumno?.examenes?.examen1); // Revisa si una propiedad existe.
 console.log("Despues de alumno");
 
 // Nullish coalescing operator (??)
+
+const pagina = 10 ?? 1
+
+console.log(pagina)
