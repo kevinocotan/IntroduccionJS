481dc528d8c9150d71296e160272e4684f947c57
kevinocotan <kaocotan@gmail.com>
2024-12-06 12:14:06 -0600

    Optional chaining

---
+++ js/22.js
@@ -0,0 +1,16 @@
+// Optional chaining ' ? '
+
+const alumno = {
+  nombre: "Juan",
+  clase: "Programacion 1",
+  aprobado: true,
+  examenes: {
+    examen1: 90,
+  },
+};
+
+console.log(alumno?.examenes?.examen1); // Revisa si una propiedad existe.
+
+console.log("Despues de alumno");
+
+// Nullish coalescing operator (??)
