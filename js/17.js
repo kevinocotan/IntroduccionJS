57d51d9230cd967a9a16b9f54de5b76a7c29ee32
kevinocotan <kaocotan@gmail.com>
2024-12-06 11:26:22 -0600

    array method every

--- js/17.js
+++ js/17.js
@@ -1,5 +1,5 @@
 const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js"];
-const numeros = [10, 20, 30]
+const numeros = [10, 20, 30];
 
 // //Filter
 
@@ -7,7 +7,7 @@ const numeros = [10, 20, 30]
 
 // console.log(nuevoArray)
 
-// // Includes 
+// // Includes
 
 // const resultado = tecnologias.includes('CSS')
 
@@ -22,6 +22,10 @@ const numeros = [10, 20, 30]
 
 // Find - Devuelve el primer elemento que cumple una condicion, 30 no lo devuelve pq es el segundo elemento
 
-const resultado = numeros.find (numero => numero > 15)
- 
-console.log(resultado)
+// const resultado = numeros.find (numero => numero > 15)
+
+//Every - Retorna true o false si todos cumplen la condicion
+
+const resultado = numeros.every (numero => numero > 15) // Arroja false por que no todaos son mayor a 15.
+
+console.log(resultado);
