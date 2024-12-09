9581d6ea6191b27ecfb3d2181719fdbb96e50286
kevinocotan <kaocotan@gmail.com>
2024-12-09 12:35:17 -0600

    validacion de formularios y alertas

--- js/32.js
+++ js/32.js
@@ -7,9 +7,35 @@ formulario.addEventListener("submit", (e) => {
   const nombre = document.querySelector("#nombre").value;
   const password = document.querySelector("#password").value;
 
-  if (nombre === "" || password === "") {
-    console.log("Todos los campos son obligatorios");
-  } else {
-    console.log("Iniciando Sesion");
+  // Prevenir repeticion de alertas.
+
+  const alertaPrevia = document.querySelector(".alerta");
+  if (alertaPrevia) {
+    alertaPrevia?.remove();
   }
+
+  const alerta = document.createElement("DIV");
+  alerta.classList.add(
+    "alerta",
+    "text-white",
+    "uppercase",
+    "text-sm",
+    "text-center",
+    "p-2",
+    "font-black"
+  );
+
+  if (nombre === "" || password === "") {
+    alerta.textContent = "Todos los campos son obligatorios";
+    alerta.classList.add("bg-red-500");
+  } else {
+    alerta.textContent = "Iniciando Sesion...";
+    alerta.classList.add("bg-green-500");
+  }
+
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.remove();
+  }, 2000);
 });
