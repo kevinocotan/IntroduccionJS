e3d075c54d580c8e7d71332a34451c83b1969145
kevinocotan <kaocotan@gmail.com>
2024-12-09 10:47:48 -0600

    selectores DOM

---
+++ js/28.js
@@ -0,0 +1,13 @@
+// Selectores
+
+const heading = document.querySelector('.heading') // Retorna un elemento
+
+// console.log(heading)
+// console.log(heading.tagName)
+// console.log(heading.textContent)
+// console.log(heading.classList)
+// console.log(heading.id)
+
+const enlaces = document.querySelectorAll('.navegacion a') // Retorna todos
+
+console.log(enlaces)
