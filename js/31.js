33e5a0cb5cf639bb45e3df36c9e1f1805a8500e2
kevinocotan <kaocotan@gmail.com>
2024-12-09 11:58:39 -0600

    eventos de inputs DOM

---
+++ js/31.js
@@ -0,0 +1,19 @@
+// Eventos del DOM - Inputs
+
+const inputNombre = document.querySelector("#nombre");
+inputNombre.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+
+const inputPasword = document.querySelector('#password');
+inputPasword.addEventListener('input', functionPassword);
+
+function functionPassword() {
+  inputPasword.type = "text";
+
+  setTimeout(() => {
+    inputPasword.type = 'password'
+  }, 300);    
+
+
+}
